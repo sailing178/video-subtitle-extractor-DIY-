5087e3f144601d3a80c130229e31ae193e9f7fad
Sailing168 <89731041+sailing178@users.noreply.github.com>
2023-12-29 21:04:47 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -995,16 +995,13 @@ class SubtitleExtractor:
 
 if __name__ == '__main__':
     multiprocessing.set_start_method("spawn")
-    # 提示用户输入视频路径
-    video_path = input(f"{config.interface_config['Main']['InputVideo']}").strip()
-    # 提示用户输入字幕区域
-    try:
-        y_min, y_max, x_min, x_max = map(int, input(
-            f"{config.interface_config['Main']['ChooseSubArea']} (ymin ymax xmin xmax)：").split())
-        subtitle_area = (y_min, y_max, x_min, x_max)
-    except ValueError as e:
-        subtitle_area = None
-    # 新建字幕提取对象
-    se = SubtitleExtractor(video_path, subtitle_area)
-    # 开始提取字幕
-    se.run()
+    # 示例视频路径列表
+    video_paths = ['/kaggle/input/heaven/1.mp4', '/kaggle/input/heaven/2.mp4', '/kaggle/input/heaven/3.mp4']
+    # 预设的字幕区域坐标
+    subtitle_area = (824, 1023, 96, 1824)
+    # 对每个视频路径进行处理
+    for video_path in video_paths:
+        # 新建字幕提取对象
+        se = SubtitleExtractor(video_path, subtitle_area)
+        # 开始提取字幕
+        se.run()
